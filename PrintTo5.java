e85cde9ca378edab35cf65ee92ab5e23616592c7
Ran Ben Shlomo <ran.benshlomo@post.runi.ac.il>
2025-02-25 20:05:52 +0200

    last answer PrintTo5

--- PrintTo5.java
+++ PrintTo5.java
@@ -1,9 +1,12 @@
+
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
-        int i = 0;
-        // Complete the program's code here:
+        int[] numberArray = new int[6];
+        for (int i=0; i < numberArray.length; i++) {
+            numberArray[i] = i;
+            System.out.println(numberArray[i]);
+        }
     }
  }
